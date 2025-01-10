cfb92b0d47b32320df4da1e4f8e5aa5645836f41
Prince abrahams <ccuser@CCs-MacBook-Air.local>
2025-01-10 11:45:51 +0200

    new configs

---
+++ src/test/java/com/premichit/fullstackmovieapp/FullstackMovieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premichit.fullstackmovieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackMovieAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
