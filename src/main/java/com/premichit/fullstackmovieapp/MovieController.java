c86d0de1fe163b9c1473be7d3eec75c8de836b5b
Prince abrahams <ccuser@CCs-MacBook-Air.local>
2025-01-12 13:35:19 +0200

    new configs

--- src/main/java/com/premichit/fullstackmovieapp/MovieController.java
+++ src/main/java/com/premichit/fullstackmovieapp/MovieController.java
@@ -1,5 +1,6 @@
 package com.premichit.fullstackmovieapp;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/movies")
 public class MovieController {
 
+    @GetMapping
     public String getAllMovies(){
         return "All movies";
     }
