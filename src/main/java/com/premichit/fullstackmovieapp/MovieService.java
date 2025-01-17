fe2a4998c2a84c6296929e9d0879a6ab2b52f70a
fullstackApp <princeabrahams777@gmail.com>
2025-01-17 09:58:22 +0200

    new class

--- src/main/java/com/premichit/fullstackmovieapp/MovieService.java
+++ src/main/java/com/premichit/fullstackmovieapp/MovieService.java
@@ -1,4 +1,16 @@
 package com.premichit.fullstackmovieapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MovieService {
+    @Autowired
+    private MovieRepository repository;
+
+    public List<Movie> getAllMovies() {
+        return repository.findAll();
+    }
 }
